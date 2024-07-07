ad2965ecf2ae4144bb11d192649d0c5f8dd3e78f
Hugo <hugolindoso.c@gmail.com>
2024-07-06 22:54:39 -0300

    Remove files that are now in .gitignore

--- AmigoSecreto.Domain/ValueObjects/UserFriend.cs
+++
@@ -1,9 +0,0 @@
-using AmigoSecreto.Domain.Entity;
-
-namespace AmigoSecreto.Domain.ValueObjects;
-public class UserFriend
-{
-    public required User User { get; set; }
-    public required User Friend { get; set; }
-    public required string UserPhone { get; set; }
-}
