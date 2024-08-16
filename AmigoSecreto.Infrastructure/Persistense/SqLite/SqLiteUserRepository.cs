48f7d0edfddfed82dd1755655621608a1555a2f7
Hugo <hugolindoso.c@gmail.com>
2024-08-16 20:29:42 -0300

    feat: ajusta get user por id usando left join

--- AmigoSecreto.Infrastructure/Persistense/SqLite/SqLiteUserRepository.cs
+++ AmigoSecreto.Infrastructure/Persistense/SqLite/SqLiteUserRepository.cs
@@ -70,46 +70,54 @@ public class SqLiteUserRepository : IUserRepository
     {
         await using var connection = new SqliteConnection(_connectionString);
         var sqlPlayer = @"SELECT 
-                                id as Id, 
-                                name as Name, 
-                                phone as Phone, 
-                                group_id as GroupId
-                              FROM [user] 
-                              WHERE id = @UserId";
-        var players = await connection.QueryAsync<PlayerSqliteResponse>(
-                sqlPlayer, new { UserId = userId.ToString() }
-            );
-        var sqlGifts = @"SELECT 
-                            id as Id, 
-                            user_id as UserId,
-                            description as Description, 
-                            link as Link
-                        FROM [gift] 
-                        WHERE user_id = @UserId";
-        var giftsSqlResponse = await connection.QueryAsync<GiftsSqliteResponse>(sqlGifts, new { UserId = userId.ToString() });
+                        u.id as Id, 
+                        u.name as Name, 
+                        u.phone as Phone, 
+                        u.group_id as GroupId,
+                        g.id as GiftId,
+                        g.description as Description,
+                        g.link as Link
+                    FROM [user] u
+                    LEFT JOIN gift g ON g.user_id = u.id
+                    WHERE u.id = @UserId";
 
-        var playerSqlResponse = players.FirstOrDefault();
-        if (playerSqlResponse is null)
-        {
-            return null;
-        }
-        var gifts = giftsSqlResponse.Select(g => new Gift()
-        {
-            Id = Guid.Parse(g.GiftId),
-            UserId = Guid.Parse(g.UserId),
-            Description = g.Description,
-            Link = g.Link
-        }).ToList();
-        var player = new Player
-        {
-            Id = Guid.Parse(playerSqlResponse.Id),
-            Name = playerSqlResponse.Name,
-            Phone = playerSqlResponse.Phone,
-            GroupId = !string.IsNullOrWhiteSpace(playerSqlResponse.GroupId) ? Guid.Parse(playerSqlResponse.GroupId) : null,
-            Gifts = gifts
-        };
+        Player? player = null;
+
+        await connection.QueryAsync<PlayerSqliteResponse, GiftsSqliteResponse, Player>(
+            sqlPlayer,
+            (playerResp, giftResp) =>
+            {
+                // if (player == null)
+                player ??= new Player()
+                {
+                    Id = Guid.Parse(playerResp.Id),
+                    Name = playerResp.Name,
+                    Phone = playerResp.Phone,
+                    GroupId = Guid.Parse(playerResp.GroupId),
+                    Gifts = []
+                };
+
+                if (giftResp != null)
+                {
+                    var gift = new Gift()
+                    {
+                        Id = Guid.Parse(giftResp.GiftId),
+                        Description = giftResp.Description,
+                        Link = giftResp.Link,
+                        UserId = Guid.Parse(playerResp.Id)
+                    };
+                    player.Gifts.Add(gift);
+                }
+
+                return player;
+            },
+            new { UserId = userId.ToString() },
+            splitOn: "GiftId"
+        );
+
         return player;
     }
+
 
     public List<Player> GetPlayers()
     {
